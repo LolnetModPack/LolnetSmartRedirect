d0ffc4147e68cace9806ba3814b86455fc79e6bf
James Anderson <james137137@gmail.com>
2015-07-16 08:04:45 +1200

    cleaning for prep of new API changes to get modpack list directly

--- src/main/java/com/james137137/LolnetSmartRedirect/LolnetSmartRedirect.java
+++ src/main/java/com/james137137/LolnetSmartRedirect/LolnetSmartRedirect.java
@@ -29,13 +29,8 @@ import nz.co.lolnet.lolnetfourmpermissionbcbridge.LolnetFourmPermissionBCBridge;
  */
 public class LolnetSmartRedirect extends Plugin {
 
-    public static HashMap<String, PlayerLoginData> playerMap;
     static Logger log;
-    private static final int PORT = 10009;
-    InfomationListener Listener;
     static LolnetSmartRedirect plugin;
-    public static boolean run = false;
-    static ServerSocket serverSocket = null;
     public static Configuration config;
 
     public static boolean isfourmRegistered(String userName) {
@@ -50,14 +45,12 @@ public class LolnetSmartRedirect extends Plugin {
     @Override
     public void onEnable() {
         setupConfigFile();
-        playerMap = new HashMap<>();
         getProxy().getPluginManager().registerListener(this, new MyListener(this));
         getProxy().getPluginManager().registerCommand(this, new Commands.LolnetSmartRedirectReload(this));
         plugin = this;
         log = this.getLogger();
-        Listener = new InfomationListener();
     }
-    
+
     private void setupConfigFile() {
         if (!getDataFolder().exists()) {
             getDataFolder().mkdir();
@@ -95,122 +88,14 @@ public class LolnetSmartRedirect extends Plugin {
 
     @Override
     public void onDisable() {
-        Listener = null;
     }
-    
-     public void reload() {
+
+    public void reload() {
         File configFile = new File(getDataFolder(), "config.yml");
         try {
             LolnetSmartRedirect.config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
         } catch (IOException ex) {
             Logger.getLogger(LolnetFourmPermissionBCBridge.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private static class InfomationListener {
-
-        public InfomationListener() {
-
-            try {
-                serverSocket = new ServerSocket(LolnetSmartRedirect.PORT);
-                log.info("listening on port: " + LolnetSmartRedirect.PORT + " for server messages.");
-                run = true;
-            } catch (IOException e) {
-                log.warning("Could not listen on port: " + LolnetSmartRedirect.PORT);
-                return;
-            }
-
-            new ThreadListenForClients();
-        }
-    }
-
-    private static class ThreadListenForClients {
-
-        public ThreadListenForClients() {
-            start();
-        }
-
-        private void start() {
-            LolnetSmartRedirect.plugin.getProxy().getScheduler().runAsync(LolnetSmartRedirect.plugin, new Runnable() {
-
-                @Override
-                public void run() {
-                    while (run) {
-                        Socket clientSocket = null;
-
-                        try {
-                            clientSocket = serverSocket.accept();
-                            new ThreadListenClient(clientSocket);
-                        } catch (IOException e) {
-                        }
-                    }
-                }
-
-            });
-        }
-
-        private static class ThreadListenClient {
-
-            Socket clientSocket;
-            BufferedReader in;
-            PrintWriter out;
-
-            public ThreadListenClient(final Socket clientSocket) {
-
-                LolnetSmartRedirect.plugin.getProxy().getScheduler().runAsync(LolnetSmartRedirect.plugin, new Runnable() {
-
-                    @Override
-                    public void run() {
-                        try {
-                            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                            out = new PrintWriter(clientSocket.getOutputStream(), true);
-                        } catch (IOException ex) {
-                            Logger.getLogger(InfomationListener.class.getName()).log(Level.SEVERE, null, ex);
-                            return;
-                        }
-                        try {
-                            String inputLine;
-                            String playerName = null;
-                            while ((inputLine = in.readLine()) != null) {
-                                if (inputLine.contains("~~~")) {
-                                    String[] split = inputLine.split("~~~");
-                                    if (split.length == 4) {
-                                        UUID playerUUID = UUID.fromString(split[0]);
-                                        playerName = split[1];
-                                        String modPackName = split[2];
-                                        String data = split[3];
-                                        data = data.replace("[", "").replace("]", "");
-                                        List<String> modList = Arrays.asList(data.split(", ", -1));
-                                        PlayerLoginData playerLoginData = new PlayerLoginData(playerName, modPackName, modList);
-                                        LolnetSmartRedirect.playerMap.put(playerName, playerLoginData);
-                                    }
-                                }
-                                if (playerName == null) {
-                                    out.println("MessageFormatError");
-                                } else {
-                                    if (LolnetSmartRedirect.isfourmRegistered(playerName)) {
-                                        out.println("Registered");
-                                    }
-                                    else
-                                    {
-                                        out.println("NotRegistered");
-                                    }
-                                }
-
-                                
-                            }
-                        } catch (Exception ex) {
-                        }
-                        try {
-                            in.close();
-                            out.close();
-                            clientSocket.close();
-                        } catch (IOException ex) {
-                            Logger.getLogger(InfomationListener.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }
-                });
-            }
         }
     }
 
--- src/main/java/com/james137137/LolnetSmartRedirect/MyListener.java
+++ src/main/java/com/james137137/LolnetSmartRedirect/MyListener.java
@@ -23,58 +23,14 @@ import net.md_5.bungee.event.EventPriority;
  */
 public class MyListener implements Listener {
 
-    private List<String> firstTimeLogin;
-
     public MyListener(LolnetSmartRedirect aThis) {
-        firstTimeLogin = new ArrayList<>();
-    }
 
-    @EventHandler
-    public void onPlayerLogin(PreLoginEvent event) {
-        String playerName = event.getConnection().getName();
-
-        if (LolnetSmartRedirect.playerMap.containsKey(playerName)) {
-
-            firstTimeLogin.add(playerName);
-        }
     }
 
     @EventHandler
     public void onPlayerLogin(ServerConnectEvent event) {
         ProxiedPlayer player = event.getPlayer();
-        if (firstTimeLogin.contains(player.getName()) && LolnetSmartRedirect.playerMap.get(player.getName()) != null) {
-            firstTimeLogin.remove(player.getName());
-            ServerInfo target = event.getTarget();
-
-            if (target.getName().equalsIgnoreCase("lobby") || target.getName().equalsIgnoreCase("lobby2")) {
-                PlayerLoginData playerData = LolnetSmartRedirect.playerMap.get(player.getName());
-                if (System.currentTimeMillis() - playerData.lastUpdate >= 5 * 1000) {
-                    return;
-                }
-                ServerInfo serverInfo = LolnetSmartRedirect.plugin.getProxy().getServers().get(playerData.modPackName);
-                if (serverInfo != null) {
-                    event.setTarget(serverInfo);
-                } else {
-                    if (playerData.modPackName.equals("ArnsForgotSomethingHere") && playerData.modList.contains("lotr")) {
-                        serverInfo = LolnetSmartRedirect.plugin.getProxy().getServers().get("TolkienCraft");
-                        if (serverInfo != null) {
-                            event.setTarget(serverInfo);
-                        }
-                    }
-                }
-            }
-        } else {
-            if (event.getTarget().getName().equalsIgnoreCase("lobby")) {
-                PlayerLoginData playerData = LolnetSmartRedirect.playerMap.get(player.getName());
-                if (playerData == null || playerData.lastUpdate >= 5 * 1000) {
-                    return;
-                }
-                if (playerData != null && playerData.modPackName.contains("tolkien")) {
-                    event.setTarget(LolnetSmartRedirect.plugin.getProxy().getServers().get("lobby3"));
-                }
-            }
-        }
-        LolnetSmartRedirect.playerMap.put(player.getName(), null);
+        //event.setTarget(serverInfo);
 
         //check if member
         if (event.getTarget().getName().contains("tolkien") && !LolnetSmartRedirect.isfourmRegistered(player.getName())) {
@@ -82,8 +38,7 @@ public class MyListener implements Listener {
             //event.getPlayer().sendMessage(ChatColor.GREEN + "Please Register on fourm");
             //event.setTarget(LolnetSmartRedirect.plugin.getProxy().getServers().get("lobby3"));
         }
-        if (LolnetSmartRedirect.config.getStringList("MemberOnlyServers").contains(event.getTarget().getName()) && !LolnetSmartRedirect.isfourmRegistered(player.getName()))
-        {
+        if (LolnetSmartRedirect.config.getStringList("MemberOnlyServers").contains(event.getTarget().getName()) && !LolnetSmartRedirect.isfourmRegistered(player.getName())) {
             event.setCancelled(true);
             event.getPlayer().sendMessage(ChatColor.GREEN + "Please Register on fourm: lolnet.co.nz before entering " + event.getPlayer().getName());
         }
